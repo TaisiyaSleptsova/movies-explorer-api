42f06e6674b5862896d10667f34607b364494e54
Taisiya Sleptsova <tasyushka@mail.ru>
2023-10-12 11:32:22 +0300

    вернула сервер на 3000. Откорректирован файл readme

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const errorServer = require('./middlewares/errorServer');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
